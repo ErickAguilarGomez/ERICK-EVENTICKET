7ffcae53b5e19350bccb8c4bb555322d339d4b03
Erick Aguilar Gomez <65264358+ErickAguilarGomez@users.noreply.github.com>
2024-10-02 19:02:40 +0000

    fixed db

--- src/app.py
+++ src/app.py
@@ -76,7 +76,7 @@ def sitemap():
         return generate_sitemap(app)
     return send_from_directory(static_file_dir, 'index.html')
 
-# any other endpoint will try to serve it like a static file
+# any other endpoint will try to serve it like a static file   .
 
 
 @app.route('/<path:path>', methods=['GET'])
